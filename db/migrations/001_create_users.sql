4559180426507e9927e124c9df5bbd0c2987e896
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-04 00:56:06 +0900

    fix: scheme

--- db/migrations/001_create_users.sql
+++ db/migrations/001_create_users.sql
@@ -2,6 +2,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     line_user_id VARCHAR(255) UNIQUE NOT NULL,
+    selected_city_id VARCHAR(10) REFERENCES area_class20(id),
+    notify_time TIME NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
