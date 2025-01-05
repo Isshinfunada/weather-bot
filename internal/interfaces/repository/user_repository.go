612fd9bbdcffe09c91eba2315569a7649ea06113
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-05 23:09:51 +0900

    add: user_repository FindByID

--- internal/interfaces/repository/user_repository.go
+++ internal/interfaces/repository/user_repository.go
@@ -11,6 +11,7 @@ import (
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
+	FindUserByID(ctx context.Context, userID int) (*entity.User, error)
 }
 
 type userRepository struct {
@@ -21,6 +22,8 @@ func NewUserRepository(db *sql.DB) UserRepository {
 	return &userRepository{db: db}
 }
 
+// CreateUserはusersテーブルに新規レコードを挿入し、
+// 作成したレコードのID　を取得して戻り値として返します
 func (r *userRepository) CreateUser(ctx context.Context, user *entity.User) (*entity.User, error) {
 	query := `
 	INSERT INTO users (line_user_id, selected_area_id, notify_time, is_active, created_at, updated_at)
@@ -50,4 +53,36 @@ func (r *userRepository) CreateUser(ctx context.Context, user *entity.User) (*en
 
 	user.ID = newID
 	return user, nil
+}
+
+// FindUserByIDはusersテーブルを検索し、見つかったらUserを返します
+func (r *userRepository) FindUserByID(ctx context.Context, userID int) (*entity.User, error) {
+	query := `
+		SELECT
+            id, line_user_id, selected_area_id, notify_time,
+            is_active, created_at, updated_at
+        FROM users
+        WHERE id = $1
+        LIMIT 1
+	`
+	row := r.db.QueryRowContext(ctx, query, userID)
+
+	var u entity.User
+	err := row.Scan(
+		&u.ID,
+		&u.LINEUserID,
+		&u.SelectedAreaID,
+		&u.NotifyTime,
+		&u.IsActive,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to get user by id: %w", err)
+	}
+
+	return &u, nil
 }
