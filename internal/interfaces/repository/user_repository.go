66ff37656404820229a6a30f6ad9149d8f86a5f0
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-06 21:58:35 +0900

    add: user_repository FindByLINEUserID

--- internal/interfaces/repository/user_repository.go
+++ internal/interfaces/repository/user_repository.go
@@ -12,6 +12,7 @@ import (
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 	FindUserByID(ctx context.Context, userID int) (*entity.User, error)
+	FindUserByLINEUserID(ctx context.Context, LINEUserID string) (*entity.User, error)
 	UpdateUser(ctx context.Context, user *entity.User) error
 	DeleteUser(ctx context.Context, userID int) error
 }
@@ -87,6 +88,38 @@ func (r *userRepository) FindUserByID(ctx context.Context, userID int) (*entity.
 	}
 
 	return &u, nil
+}
+
+func (r *userRepository) FindUserByLINEUserID(ctx context.Context, LINEUserID string) (*entity.User, error) {
+	query := `
+		SELECT
+			id, line_user_id, selected_area_id, notify_time,
+			is_active, created_at, updated_at
+		FROM users
+		WHERE line_user_id = $1
+		LIMIT 1
+	`
+
+	row := r.db.QueryRowContext(ctx, query, LINEUserID)
+
+	var u entity.User
+	err := row.Scan(
+		&u.ID,
+		&u.LINEUserID,
+		&u.SelectedAreaID,
+		&u.NotifyTime,
+		&u.IsActive,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to get user by LINEUserID: %w", err)
+	}
+	return &u, nil
+
 }
 
 func (r *userRepository) UpdateUser(ctx context.Context, user *entity.User) error {
