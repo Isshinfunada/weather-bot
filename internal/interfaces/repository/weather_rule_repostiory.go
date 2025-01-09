2e333fdc35e0c7c9014bb1106eadf6066416d7f8
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-09 22:09:15 +0900

    add: repository weather_rule_repository

---
+++ internal/interfaces/repository/weather_rule_repostiory.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/Isshinfunada/weather-bot/internal/entity"
+)
+
+type WeatherRuleRepository interface {
+	GetRule(ctx context.Context, weatherCode string) (*entity.WeatherRule, error)
+}
+
+type weatherRuleRepository struct {
+	db *sql.DB
+}
+
+func NewWeatherRuleRepository(db *sql.DB) WeatherRuleRepository {
+	return &weatherRuleRepository{db: db}
+}
+
+func (r *weatherRuleRepository) GetRule(ctx context.Context, weatherCode string) (*entity.WeatherRule, error) {
+	query := `
+	SELECT weather_code, weather_description, is_notify_trigger
+	FROM weather_description_rules
+	WHERE weather_code = $1
+	`
+
+	var rule entity.WeatherRule
+	err := r.db.QueryRowContext(ctx, query, weatherCode).Scan(
+		&rule.WeatherCode, &rule.WeatherDescription, &rule.IsNotifyTrigger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get weather rule: %w", err)
+	}
+	return &rule, nil
+}
